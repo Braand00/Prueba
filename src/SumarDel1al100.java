302664ece2e9ce1e4e5d362c38630dbe9e7ecb08
unknown <brando10@educa.madrid.org>
2023-01-17 21:05:31 +0100

    version1

---
+++ src/SumarDel1al100.java
@@ -0,0 +1,16 @@
+
+public class SumarDel1al100 {
+
+	public static void main(String[] args) {
+		
+		int suma=0;
+	
+		for (int i=1; i<=100; i++) {
+			suma=suma +i;
+		}
+		// Escribir
+		System.out.println(suma);
+						
+	}
+		
+}
